e58d12ab3bc564cbfce3a1129cf535372cda5810
Leonardo Bortolini <neijoleo@gmail.com>
2025-04-10 22:03:54 -0300

    Change medico adapter

--- src/main/java/com/fiap/consultas/infraestructure/http/MedicoServiceAdapter.java
+++ src/main/java/com/fiap/consultas/infraestructure/http/MedicoServiceAdapter.java
@@ -2,6 +2,9 @@ package com.fiap.consultas.infraestructure.http;
 
 import com.fiap.consultas.application.dtos.MedicoDTO;
 import com.fiap.consultas.application.ports.MedicoServicePort;
+import com.fiap.consultas.domain.entities.HorarioTrabalho;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -10,6 +13,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -32,14 +38,74 @@ public class MedicoServiceAdapter implements MedicoServicePort {
                 .queryParam("cidade", cidade)
                 .toUriString();
 
-        ResponseEntity<List<MedicoDTO>> response = restTemplate.exchange(
+        ResponseEntity<List<MedicoCustom>> response = restTemplate.exchange(
                 url,
                 HttpMethod.GET,
                 null,
-                new ParameterizedTypeReference<>() {
-                }
+                new ParameterizedTypeReference<>() { }
         );
 
-        return response.getBody();
+        List<MedicoCustom> medicos = response.getBody();
+        return medicos != null ? medicos.stream().map(this::convertToMedicoDTO).toList() : Collections.emptyList();
+    }
+
+    private MedicoDTO convertToMedicoDTO(MedicoCustom medico) {
+        MedicoDTO medicoDTO = new MedicoDTO();
+        medicoDTO.setId(medico.getId().toString());
+        medicoDTO.setNome(medico.getNome());
+        medicoDTO.setEspecialidade(medico.getEspecialidade());
+        medicoDTO.setCidade(medico.getCidade());
+
+        List<HorarioTrabalho> horariosConvertidos = medico.getHorariosTrabalho().stream()
+                .map(this::convertHorarioTrabalho)
+                .toList();
+
+        medicoDTO.setHorariosTrabalho(horariosConvertidos);
+
+        return medicoDTO;
+    }
+
+    private HorarioTrabalho convertHorarioTrabalho(HorarioTrabalhoCustom horarioOriginal) {
+        HorarioTrabalho horarioDTO = new HorarioTrabalho();
+
+        horarioDTO.setHoraInicio(horarioOriginal.getHoraInicio());
+        horarioDTO.setHoraFim(horarioOriginal.getHoraFim());
+        horarioDTO.setDiaSemana(convertDiaDaSemana(horarioOriginal.getDiaDaSemana()));
+
+        return horarioDTO;
+    }
+
+    private DayOfWeek convertDiaDaSemana(DiaDaSemanaCustom diaDaSemana) {
+        return switch (diaDaSemana) {
+            case SEGUNDA -> DayOfWeek.MONDAY;
+            case TERCA -> DayOfWeek.TUESDAY;
+            case QUARTA -> DayOfWeek.WEDNESDAY;
+            case QUINTA -> DayOfWeek.THURSDAY;
+            case SEXTA -> DayOfWeek.FRIDAY;
+            case SABADO -> DayOfWeek.SATURDAY;
+            case DOMINGO -> DayOfWeek.SUNDAY;
+        };
+    }
+
+    @Getter
+    @Setter
+    private static class MedicoCustom {
+        private Long id;
+        private String nome;
+        private String especialidade;
+        private String cidade;
+        private List<HorarioTrabalhoCustom> horariosTrabalho;
+    }
+
+    @Getter
+    @Setter
+    private static class HorarioTrabalhoCustom {
+        private DiaDaSemanaCustom diaDaSemana;
+        private LocalTime horaInicio;
+        private LocalTime horaFim;
+    }
+
+    private enum DiaDaSemanaCustom {
+        SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO
     }
 }
